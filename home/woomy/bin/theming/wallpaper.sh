f30474cb9650d6d1d0f8085f90063d5461399df9
Woomymy <github@woomy.ovh>
2020-12-09 18:35:41 +0100

    mercredi 09 décembre 18:35  2020

--- home/woomy/bin/theming/wallpaper.sh
+++ home/woomy/bin/theming/wallpaper.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Set random wallpaper
 WALLPATH="$HOME/Pictures/wallpapers"
+if [[ ! -d "${WALLPATH}" ]] 
+then
+	notify-send -u critical "WALLPAPER DIR NOT FOUND!" ${WALLPATH}
+	exit 1
+fi
 files=($WALLPATH/*)
 num=${#files[@]}
 wall="${files[RANDOM % num]}"
-feh --bg-fill "${wall}"
 wal -i "${wall}"

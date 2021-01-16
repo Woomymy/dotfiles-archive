ae5b462b52835708070903b5b5d8291c94dfbc94
Woomy4680-exe <matzar1808@gmail.com>
2021-01-16 15:05:49 +0100

    💄 Add blue theme

--- home/bin/theming/setup-themes.sh
+++ home/bin/theming/setup-themes.sh
@@ -3,10 +3,15 @@ if [[ ! -d "$HOME/wallpapers" ]]
 then
     exit 1
 fi
-COLORS=(blue darkblue darkblue-alt green grey lightblue lightgreen lightturquoise orange pink purple red turquoise yellow)
-NUM=${#COLORS[@]}
-COLOR="${COLORS[RANDOM % NUM]}"
-COLOR="green"
+COLOR=""
+if [[ -f "${HOME}/wallpapers/${1}.jpg" ]]
+then
+    COLOR=$1
+else 
+    COLORS=(blue darkblue darkblue-alt green grey lightblue lightgreen lightturquoise orange pink purple red turquoise yellow)
+    NUM=${#COLORS[@]}
+    COLOR="${COLORS[RANDOM % NUM]}"
+fi
 if [[ ! -f "$HOME/wallpapers/${COLOR}.jpg" ]]
 then
     exit 1

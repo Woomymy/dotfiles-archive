86bb271d05f41f9d15e97281ba52ebc9e8386ed2
Woomy4680-exe <matzar1808@gmail.com>
2021-01-18 20:20:51 +0100

    ✨ Add colors script

--- home/bin/theming/colors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source "${HOME}/bin/config/colors.sh"
-OPTS=""
-for COL in ${COLORS[*]}
-do
-	OPTS="${OPTS}${COL}\n"
-done
-CH="$(echo -e "$OPTS" | rofi -dmenu)"
-~/bin/wm/theming.sh $CH

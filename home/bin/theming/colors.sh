f93b6d8da665b14c09bc0e1769e29e8f5e6c7831
Woomymy <github@woomy.ovh>
2021-01-24 13:15:54 +0100

    :ambulance: Fix colors script

--- home/bin/theming/colors.sh
+++ home/bin/theming/colors.sh
@@ -18,46 +18,46 @@ opts="$blue\n$darkblue\n${darkblue-alt}\n${green}\n${gray}\n$lightblue\n$lightgr
 ch="$(echo -e "$opts" | rofi -dmenu)"
 case $ch in
     $blue)
-        $HOME/bin/theming/setup-themes.sh $blue
+        $HOME/bin/theming/setup-themes.sh "blue"
         ;;
     $darkblue)
-        $HOME/bin/theming/setup-themes.sh $darkblue
+        $HOME/bin/theming/setup-themes.sh "darkblue"
         ;;
-    ${darkblue-alt})
-        $HOME/bin/theming/setup-themes.sh ${darkblue-alt}
+    "${darkblue-alt}")
+        $HOME/bin/theming/setup-themes.sh "darkblue-alt"
         ;;
     $green)
-        $HOME/bin/theming/setup-themes.sh $green
+        $HOME/bin/theming/setup-themes.sh "green"
         ;;
     $gray)
-        $HOME/bin/theming/setup-themes.sh $gray
+        $HOME/bin/theming/setup-themes.sh "gray"
         ;;
     $lightblue)
-        $HOME/bin/theming/setup-themes.sh $lightblue
+        $HOME/bin/theming/setup-themes.sh "lightblue"
         ;;
     $lightgreen)
-        $HOME/bin/theming/setup-themes.sh $lightgreen
+        $HOME/bin/theming/setup-themes.sh "lightgreen"
         ;;
     $lightturquoise)
-        $HOME/bin/theming/setup-themes.sh $lightturquoise
+        $HOME/bin/theming/setup-themes.sh "lightturquoise"
         ;;
     $orange)
-        $HOME/bin/theming/setup-themes.sh $orange
+        $HOME/bin/theming/setup-themes.sh "orange"
         ;;
     $pink)
-        $HOME/bin/theming/setup-themes.sh $pink
+        $HOME/bin/theming/setup-themes.sh "pink"
         ;;
     $purple)
-        $HOME/bin/theming/setup-themes.sh $purple
+        $HOME/bin/theming/setup-themes.sh "purple"
         ;;
     $red)
-        $HOME/bin/theming/setup-themes.sh $red
+        $HOME/bin/theming/setup-themes.sh "red"
         ;;
     $turquoise)
-        $HOME/bin/theming/setup-themes.sh $turquoise
+        $HOME/bin/theming/setup-themes.sh "turquoise"
         ;;
     $yellow)
-        $HOME/bin/theming/setup-themes.sh $yellow
+        $HOME/bin/theming/setup-themes.sh "yellow"
         ;;    
 esac
 

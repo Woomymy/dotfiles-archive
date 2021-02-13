658773ad8b85aeff374915f5adb44a4352de4c36
Woomymy <github@woomy.ovh>
2021-02-13 13:08:42 +0100

    :lipstick: Update gray polybar

---
+++ home/bin/lib/colors.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+get_color() {
+    case $1 in 
+        "blue")
+            COLOR="#1E88E5"
+            ;;
+        "darkblue")
+            COLOR="#5E35B1"
+            ;;
+        "green")
+            COLOR="#43A047"
+            ;;
+        "gray")
+            COLOR="#37474F"
+            ;;
+        "lightblue")
+            COLOR="#039BE5"
+            ;;
+        "lightgreeen")
+            COLOR="#7CB342"
+            ;;
+        "lightturquois")
+            COLOR="#0097A7"
+            ;;
+        "orange")
+            COLOR="#FF6F00"
+            ;;
+        "pink")
+            COLOR="#D81B60"
+            ;;
+        "purple")
+            COLOR="#8E24AA"
+            ;;
+        "red")
+            COLOR="#E53935"
+            ;;
+        "turquoise")
+            COLOR="#00897B"
+            ;;
+        "yellow")
+            COLOR="#FBC02D"
+            ;;
+        esac
+        
+}

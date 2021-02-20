a7c5e38fb80e14e38274f21191b9ae220327b952
Woomy4680-exe <matzar1808@gmail.com>
2021-02-20 18:19:46 +0100

    🐛 Fix extensions backup path

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -4,4 +4,4 @@ echo -e "\e[96m[POSTSAVE] Saving VScode extensions\e[m" # Follow kelpdot style f
     echo -e "\e[91mError: VSCode not found!\e[m"
     exit 1
 }
-code --list-extensions > ./home/.config/Code/extensions # Backup the extensions list
+code --list-extensions > ./home/.config/Code/User/extensions # Backup the extensions list

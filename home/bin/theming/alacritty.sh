53c1117352f94f37ed098057c9b19199b080e294
Woomy4680-exe <matzar1808@gmail.com>
2021-02-22 20:08:41 +0100

    :bug: Backup my dotfiles if I broke my system while installing ecryptfs

---
+++ home/bin/theming/alacritty.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source "${HOME}/bin/lib/colors.sh"
+get_color $1
+TPL="${HOME}/bin/theming/templates/alacritty.yml"
+if [[ ! -f "${TPL}" ]]
+then 
+    echo "${TPL} not found!"
+    exit 1
+fi
+TMP_TPL="/tmp/alacritty.yml"
+cp "${TPL}" "${TMP_TPL}"
+sed -i "s/\@CURSOR_COLOR\@/${COLOR}/g" "${TMP_TPL}"
+cp "${TMP_TPL}" "${HOME}/.config/alacritty/alacritty.yml"

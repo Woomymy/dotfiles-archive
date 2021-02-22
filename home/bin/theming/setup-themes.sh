b0de0677150e25cb58556ee3e9572c5ddd9ca061
Woomy4680-exe <matzar1808@gmail.com>
2021-02-22 20:08:41 +0100

    :bug: Backup my dotfiles if I broke my system while installing ecryptfs

---
+++ home/bin/theming/setup-themes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+if [[ ! -d "$HOME/wallpapers" ]]
+then
+    exit 1
+fi
+COLOR=""
+if [[ -f "${HOME}/wallpapers/${1}.jpg" ]]
+then
+    COLOR=$1
+else 
+    COLORS=(blue darkblue green lightblue lightgreen lightturquoise orange pink purple red turquoise yellow)
+    NUM=${#COLORS[@]}
+    COLOR="${COLORS[RANDOM % NUM]}"
+fi
+if [[ ! -f "$HOME/wallpapers/${COLOR}.jpg" ]]
+then
+    exit 1
+fi
+feh --bg-fill "${HOME}/wallpapers/${COLOR}.jpg"
+$HOME/bin/theming/alacritty.sh $COLOR
+$HOME/.config/polybar/launch.sh $COLOR

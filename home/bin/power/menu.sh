450d5a5be732050b9e28d39dcc4c602b13a9fff1
Woomy <woomy@woomy.be>
2021-03-02 11:01:09 +0100

    Lock: Add I3lock configuration

--- home/bin/power/menu.sh
+++ home/bin/power/menu.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-power_off=" Power Off"
+power_off="⏻ Power Off"
 reboot="⭘ Reboot"
-log_out=" Logout"
-opts="$power_off\n$reboot\n$log_out"
+log_out=" Logout"
+lock=" Lock"
+opts="$lock\n$power_off\n$reboot\n$log_out"
 
 ch="$(echo -e "$opts" | rofi -dmenu)"
 case $ch in
+    $lock)
+	    i3lock -c $(cat /tmp/i3lock_color)
+	    ;;
     $power_off)
-	loginctl poweroff
+	    loginctl poweroff
 	;;
     $reboot)
         loginctl reboot

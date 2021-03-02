7b883fef65f820dfed7cf8521eb9cb374ce7a2de
Woomy4680-exe <matzar1808@gmail.com>
2021-03-02 21:12:36 +0100

    Format: Lint shell scripts

--- home/bin/lib/colors.sh
+++ home/bin/lib/colors.sh
@@ -3,52 +3,52 @@
 get_color() {
     case $1 in 
         "blue")
-            COLOR="#1E88E5"
-	    BG="#F1F5F9"
+            export COLOR="#1E88E5"
+	    export BG="#F1F5F9"
 	    ;;
         "darkblue")
-            COLOR="#5E35B1"
-	    BG="#F1F5F9"
+            export COLOR="#5E35B1"
+	    export BG="#F1F5F9"
 	    ;;
         "green")
-            COLOR="#43A047"
-	    BG="#F1F5F9"
+            export COLOR="#43A047"
+	    export BG="#F1F5F9"
 	    ;;
         "lightblue")
-            COLOR="#039BE5"
-	    BG="#F1F5F9"
+            export COLOR="#039BE5"
+	    export BG="#F1F5F9"
 	    ;;
         "lightgreen")
-            COLOR="#7CB342"
-	    BG="#F1F5F9"
+            export COLOR="#7CB342"
+	    export BG="#F1F5F9"
 	    ;;
         "lightturquoise")
-            COLOR="#0097A7"
-	    BG="#F1F5F9"
+            export COLOR="#0097A7"
+	    export BG="#F1F5F9"
 	    ;;
         "orange")
-            COLOR="#FF6F00"
-	    BG="#F1F5F9"
+            export COLOR="#FF6F00"
+	    export BG="#F1F5F9"
 	    ;;
         "pink")
-            COLOR="#D81B60"
-            BG="#F1F5F9"
+            export COLOR="#D81B60"
+            export BG="#F1F5F9"
 	    ;;
         "purple")
-            COLOR="#8E24AA"
-            BG="#F1F5F9"
+            export COLOR="#8E24AA"
+            export BG="#F1F5F9"
 	    ;;
         "red")
-            COLOR="#E53935"
-            BG="#F1F5F9"
+            export COLOR="#E53935"
+            export BG="#F1F5F9"
 	    ;;
         "turquoise")
-            COLOR="#00897B"
-            BG="#F1F5F9"
+            export COLOR="#00897B"
+            export BG="#F1F5F9"
 	    ;;
         "yellow")
-            COLOR="#FBC02D"
-            BG="#333"
+            export COLOR="#FBC02D"
+            export BG="#333"
 	    ;;
         esac
         

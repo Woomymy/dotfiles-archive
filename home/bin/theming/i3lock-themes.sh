8857d02c89550bd2e2019f92dc3fafba23ea3de5
Woomy4680-exe <matzar1808@gmail.com>
2021-03-02 21:31:56 +0100

    Theming: Remove unused rofi theming script

--- home/bin/theming/i3lock-themes.sh
+++ home/bin/theming/i3lock-themes.sh
@@ -3,6 +3,6 @@
 WALL="${HOME}/wallpapers/${1}.jpg"
 [[ -f "${WALL}" ]] || exit 1
 source "${HOME}/bin/lib/colors.sh"
-get_color $1
-COLOR="$(echo $COLOR | cut -d "#" -f 2)"
+get_color "${1}"
+COLOR="$(echo "${COLOR}" | cut -d "#" -f 2)"
 echo "${COLOR}" > /tmp/i3lock_color

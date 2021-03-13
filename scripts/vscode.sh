9391e78d8b9ee6df54d58bbc9ffe7d8f122fa946
Woomy4680-exe <matzar1808@gmail.com>
2021-03-13 14:31:57 +0100

    Neovim: Add ALE configuration

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+ROOT=$1
+[[ -z "${ROOT}" ]] && ROOT="."
 echo -e "\e[96m[POSTSAVE] Saving VScode extensions\e[m" # Follow kelpdot style for messages
 [ "$(command -v code)" ] || {
     echo -e "\e[91mError: VSCode not found!\e[m"
     exit 1
 }
-code --list-extensions > ./home/.config/Code/User/extensions # Backup the extensions list
+code --list-extensions > "${ROOT}/home/.config/Code/User/extensions" # Backup the extensions list

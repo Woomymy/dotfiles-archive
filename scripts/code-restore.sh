125b5a3ecd8d7e7ed90043acb2d4ba73020e928e
Woomymy <github@woomy.ovh>
2021-03-24 14:11:54 +0100

    VSCode: Remove VScode backup scripts

--- scripts/code-restore.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo -e "\e[96m[POSTINSTALL] Restoring VSCode extensions..."
-[[ "$(command -v code)" ]] && {
-    cat home/.config/Code/User/extensions | xargs -n 1 code --install-extension
-}

8af269788f7337cbb913ab38cb17f71865425dbf
Woomy4680-exe <matzar1808@gmail.com>
2021-03-24 14:11:54 +0100

    VSCode: Remove VScode backup scripts

--- scripts/vscode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ROOT=$1
-[[ -z "${ROOT}" ]] && ROOT="."
-echo -e "\e[96m[POSTSAVE] Saving VScode extensions\e[m" # Follow kelpdot style for messages
-[ "$(command -v code)" ] || {
-    echo -e "\e[91mError: VSCode not found!\e[m"
-    exit 1
-}
-code --list-extensions > "${ROOT}/home/.config/Code/User/extensions" # Backup the extensions list

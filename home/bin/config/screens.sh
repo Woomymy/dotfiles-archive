63f820b25fb2b18597e0e4d91fbcb194430c62b6
Woomy4680-exe <matzar1808@gmail.com>
2021-04-05 13:23:15 +0200

    Theming: Rewrite a part of the setup-themes script

--- home/bin/config/screens.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Setup double screen
-
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
-

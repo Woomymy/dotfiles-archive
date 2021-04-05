f90c0f54880dd9186654b181b87a5394f941ff8f
Woomymy <github@woomy.ovh>
2021-04-05 13:23:15 +0200

    Theming: Rewrite a part of the setup-themes script

--- home/bin/wm/theming.sh
+++ home/bin/wm/theming.sh
@@ -1,2 +1,37 @@
 #!/usr/bin/env bash
+[[ -d "${HOME}/wallpapers" ]] || exit 1
+[[ -f "${HOME}/bin/config/colors.sh" ]] || { 
+	echo "Color configs not found!"
+	exit 1
+}
+# shellcheck disable=SC1090
+source "${HOME}/bin/config/colors.sh"
+source "/tmp/${USER}_startup.sh"
+ERRORS=()
+COLOR=""
+if [[ -f "${HOME}/wallpapers/${1}.jpg" ]]
+then
+	COLOR="${1}"
+else
 
+	NUM=${#COLORS[@]}
+	COLOR="${COLORS[RANDOM % NUM]}"
+fi
+if [[ ! -f "${HOME}/wallpapers/${COLOR}.jpg" ]]
+then
+	exit 1
+fi
+if [[ "$(command -v "feh")" ]]
+then
+	feh --bg-fill "${HOME}/wallpapers/${COLOR}.jpg"
+else
+	ERRORS+=("feh not found!")
+fi
+for ERR in ${ERRORS[*]}
+do
+    echo "${ERR}" >> "${STARTUP_TEMPDIR}/errors.log"
+done
+"${HOME}/bin/theming/alacritty.sh" "${COLOR}"
+"${HOME}/bin/theming/dunst.sh" "${COLOR}"
+"${HOME}/.config/polybar/launch.sh" "${COLOR}"
+"${HOME}/bin/theming/i3lock-themes.sh" "${COLOR}"

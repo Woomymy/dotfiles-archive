d9dfd26f581499b8abee5ea703f15a1100d955f4
Woomymy <github@woomy.ovh>
2021-04-05 13:51:45 +0200

    I3lock: Cleanup i3lock theming script

--- home/bin/theming/softwares/i3lock-themes.sh
+++ home/bin/theming/softwares/i3lock-themes.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 setup_theme() {
-	[[ "$(command -v convert)" ]] || exit 1
-	WALL="${HOME}/wallpapers/${1}.jpg"
-	[[ -f "${WALL}" ]] || exit 1
 	source "${HOME}/bin/lib/colors.sh"
 	get_color "${1}"
-	COLOR="$(echo "${COLOR}" | cut -d "#" -f 2)"
+	COLOR="$(echo "${COLOR}" | awk -F "#" '{print $2}')"
 	echo "${COLOR}" > /tmp/i3lock_color
 }

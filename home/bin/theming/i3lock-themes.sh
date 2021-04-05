c7b5a8d84a148fed8c51fabaae8ead14177d8947
Woomy4680-exe <matzar1808@gmail.com>
2021-04-05 13:32:29 +0200

    Theming: Execute all softwares at each restart

--- home/bin/theming/i3lock-themes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-[[ "$(command -v convert)" ]] || exit 1
-WALL="${HOME}/wallpapers/${1}.jpg"
-[[ -f "${WALL}" ]] || exit 1
-source "${HOME}/bin/lib/colors.sh"
-get_color "${1}"
-COLOR="$(echo "${COLOR}" | cut -d "#" -f 2)"
-echo "${COLOR}" > /tmp/i3lock_color

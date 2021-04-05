a1d0ae70d041547fcc6325790bd856a9d23fe25d
Woomy4680-exe <matzar1808@gmail.com>
2021-04-05 13:23:15 +0200

    Theming: Rewrite a part of the setup-themes script

---
+++ home/bin/config/colors.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Colors helpers
+
+# You can disable colors if you want
+
+export COLORS=("blue" "darkblue" "green" "lightblue" "lightgreen" "lightturquoise" "orange" "pink" "purple" "red" "turquoise" "yellow")
+

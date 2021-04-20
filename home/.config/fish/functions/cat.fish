d67fe9423606def533fce134d7e258e665832087
Woomy <woomy@woomy.be>
2021-04-20 09:01:06 +0200

    Fish: Alias cat with bat

---
+++ home/.config/fish/functions/cat.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function cat --wraps='bat --decorations=never --paging=never' --description 'alias cat=bat --decorations=never --paging=never'
+  bat --decorations=never --paging=never $argv;
+end

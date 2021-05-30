816671f7b9a4afaf6e755fe2fec2ce5b9d21df91
Woomymy <github@woomy.ovh>
2021-05-30 15:01:54 +0200

    Fish: Move gpg_tty to function

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,6 +6,3 @@ set fish_greeting ""
 set -x EDITOR nvim
 set -x GPG_TTY (tty)
 set -gx PATH $PATH ~/go/bin/ ~/.cargo/bin/ ~/.local/bin/ /usr/local/bin
-function gpg_tty --description "Reset GPG_TTY" 
-	set -x GPG_TTY (tty)
-end

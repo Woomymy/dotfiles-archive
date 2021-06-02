bb638d098bd44751cd3b92fffd0f18366614422c
Woomymy <github@woomy.ovh>
2021-06-02 18:05:13 +0200

    Nvidia-power: Warn if power_state not found

--- home/bin/power/nvidia-power.sh
+++ home/bin/power/nvidia-power.sh
@@ -11,7 +11,13 @@
 GPU_ICON="閭"
 
 DEVID="$(lspci | grep -i nvidia | awk '{print $1}')"
-if [[ "$(cat "/sys/bus/pci/devices/0000:${DEVID}/power_state")" == "D0" ]]
+SYSFS_STATE_FILE="/sys/bus/pci/devices/0000:${DEVID}/power_state"
+[[ -f "${SYSFS_STATE_FILE}" ]] || {
+	# May also append on older kernels (< 5.10 iirc, to check)
+	notify-send -u "critical" "Nvidia power module" "${SYSFS_STATE_FILE} not found, are you sure you have an NVIDIA device ? You may also need to update your kernel."
+	exit
+}
+if [[ "$(cat "${SYSFS_STATE_FILE}")" == "D0" ]]
 then
 	echo "${GPU_ICON} GPU: ON "
 else

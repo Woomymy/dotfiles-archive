b790fae1c4001bc84a67e8a69c50520a6c912865
Woomymy <github@woomy.ovh>
2021-06-10 16:51:39 +0200

    SSH: Share agents

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,4 +6,8 @@ set fish_greeting ""
 set -x EDITOR nvim
 set -x GPG_TTY (tty)
 set -gx PATH $PATH ~/go/bin/ ~/.cargo/bin/ ~/.local/bin/ /usr/local/bin
-eval (ssh-agent -c) &> /dev/null
+if not test -f "/tmp/SSH_ENV"
+	ssh-agent -c | head -n 2 > /tmp/SSH_ENV
+end
+
+source "/tmp/SSH_ENV"

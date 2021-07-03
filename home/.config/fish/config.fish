7e40106a874eb0fcee8d3891eff78b47720a2ef4
Woomy <woomy@woomy.be>
2021-07-03 18:51:39 +0200

    Fish: Add startx

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,8 @@
+if test (tty) = "/dev/tty1"
+	clear
+	exec startx &> /dev/null
+end
+
 set -gx GPG_TTY (tty) # Fixes "inapropriate Ioctl for device" errors
 set -gx PATH $PATH ~/.cargo/bin # Add cargo binaries to path
-set -gx fish_greeting ""
+set fish_greeting ""

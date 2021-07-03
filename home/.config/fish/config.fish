7df72de7ec0443926e2481e475acc25021b5cad1
Woomymy <github@woomy.ovh>
2021-07-03 19:15:06 +0200

    Fish: Start ssh-agent

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,3 +7,8 @@ set -gx GPG_TTY (tty) # Fixes "inapropriate Ioctl for device" errors
 set -gx PATH $PATH ~/.cargo/bin # Add cargo binaries to path
 set -gx EDITOR nvim
 set fish_greeting ""
+
+if not test -f "/tmp/SSH_ENV"
+	ssh-agent -c | head -n 2 > /tmp/SSH_ENV
+end
+source "/tmp/SSH_ENV"

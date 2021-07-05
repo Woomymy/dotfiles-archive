97b10e4e882fbd45e15659e2f2a53c3fa796850b
Woomy <woomy@woomy.be>
2021-07-05 09:11:17 +0200

    I3: Fix startup.sh python exec

--- home/.bin/startup.sh
+++ home/.bin/startup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-HOMEBIN="~/.bin"
 # Startup script:
 # Setup wallpapers
 # Setup applications
 
 # Setup wallpapers
 
-"${HOMEBIN}/theming/wallpapers.py"
+python "${HOME}/.bin/theming/wallpapers.py"

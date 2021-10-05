4fdcfc391a92b155f895d2538b37d6e2833fefce
Woomy <woomy@woomy.be>
2021-10-05 18:40:45 +0200

    Fish: Add config

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,13 @@
+if test (tty) = "/dev/tty1"
+	clear
+	exec startx &> /dev/null
+end
+set -gx GPG_TTY (tty) # Fixes "inapropriate Ioctl for device" errors
+set -gx PATH $PATH ~/.cargo/bin # Add cargo binaries to path
+set -gx EDITOR nvim
+set fish_greeting ""
+
+if not test -f "/tmp/SSH_ENV"
+	ssh-agent -c | head -n 2 > /tmp/SSH_ENV
+end
+source "/tmp/SSH_ENV"
---
+++ home/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,14 @@
+set red "\e[91m"
+set cyan "\e[96m"
+set magenta "\e[95m"
+set reset "\e[m"
+function fish_prompt --description "Write the prompt"
+	set prevstatus $status
+	if not test $prevstatus = "0"
+		set prompt_status "$red [$prevstatus]$reset"
+	else
+		set prompt_status ""
+	end
+	
+	echo -nse "$magenta" "$USER" "$reset" @ (prompt_hostname) "$magenta" " " (prompt_pwd) "$reset" " (" (date +%H:%M) ")" "$cyan" (fish_vcs_prompt) "$red" "$prompt_status" "$magenta" " \$ "
+end

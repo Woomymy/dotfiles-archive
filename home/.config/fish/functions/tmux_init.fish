81d6971ed7750c816817414df4bacb356ea4e57e
Woomy <woomy@woomy.be>
2021-12-13 19:33:11 +0100

    Fish: Autostart TMUX

---
+++ home/.config/fish/functions/tmux_init.fish
@@ -0,0 +1,13 @@
+function tmux_init --description "Attach to TMUX if possible"
+    if test -n "$TMUX"
+        # TMUX is already launched
+        return
+    end
+    # We need to fix that
+    if not string match -r -i '^/dev/pts/[0-9]' (tty)
+        # Refuse to launch on cassic TTYs
+        return
+    end
+    set TMUX_SESSION $USER
+    exec tmux new -t $TMUX_SESSION
+end

fa0bc98b2a252bfb19304251162237f135ba0a95
Woomy <woomy@woomy.be>
2022-02-20 11:48:20 +0100

    NVIM: Add trouble.nvim

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -2,6 +2,6 @@
 require('plugins') -- Load packer.nvim
 require('settings') -- Load vim settings
 
-for _, plugin in ipairs({"autopairs", "lualine",  "ayu", "alpha", "lsp", "nvim-tree"}) do
+for _, plugin in ipairs({"autopairs", "lualine",  "ayu", "alpha", "lsp", "nvim-tree", "trouble"}) do
     require("plugins/" .. plugin) -- Load all plugins
 end
--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -36,4 +36,5 @@ return require('packer').startup(function()
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'folke/trouble.nvim' -- Problems list
 end)

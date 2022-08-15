552152c0dc1283a5cea7f88cdb357697e90afa41
Woomy <woomy@woomy.be>
2022-08-15 15:14:55 +0200

    bin/theming: Add themes.d to generate eww scss pal
    * Eww config comming soon :tm:
    
    Signed-off-by: Woomymy <woomy@woomy.be>

---
+++ home/.bin/theming/themes.d/eww.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PAL="${1}"
+OUT="${HOME}/.config/eww/palette.scss"
+SCSS=""
+
+touch $OUT
+
+exec 1>$OUT
+
+jq -r ". | to_entries | .[].key" "${PAL}" | while read key; do
+    echo "\$${key}: $(jq -r ".${key}" "${PAL}");"
+done
+

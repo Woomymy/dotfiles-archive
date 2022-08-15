ecf9487d1bc1eed21b56f9295768b1e7b547b350
Woomy <woomy@woomy.be>
2022-08-15 15:19:47 +0200

    config/fish: Use mu_palette to theme prompt
    Signed-off-by: Woomymy <woomy@woomy.be>

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,15 +1,31 @@
-set red "\e[91m"
-set cyan "\e[96m"
-set magenta "\e[95m"
-set reset "\e[0;m"
+set error "\e[91m"
+set tertiary "\e[96m"
+set primary "\e[95m"
+set reset "\e[39m"
 set bold "\e[1m"
+
+function palget
+    string replace '#' '' (jq -r ".$argv[1]" "/tmp/mu_palette.json")
+end
+
+function update_colors --description "Update prompt colors"
+    if test -f "/tmp/mu_palette.json"
+        set primary (set_color (palget "primary"))
+        set tertiary (set_color (palget "tertiary"))
+        set error (set_color (palget "error"))
+    end
+end
+
+update_colors
+
 function fish_prompt --description "Write the prompt"
 	set prevstatus $status
-	if not test $prevstatus = "0"
-		set prompt_status "$red [$prevstatus]$reset"
+	update_colors
+    if not test $prevstatus = "0"
+		set prompt_status "$error [$prevstatus]$reset"
 	else
 		set prompt_status ""
 	end
-	
-	echo -nse "$magenta" "$USER" "$reset" @ (prompt_hostname) "$magenta" " " (prompt_pwd) "$reset" " (" (date +%H:%M) ")" "$cyan$bold" (fish_vcs_prompt) "$reset$red" "$prompt_status" "$magenta" " \$ "
+
+	echo -nse "$primary" "$USER" "$reset" @ (prompt_hostname) "$primary" " " (prompt_pwd) "$reset" " (" (date +%H:%M) ")" "$tertiary$bold" (fish_vcs_prompt) "$reset" "$prompt_status" "$primary" " \$$reset "
 end

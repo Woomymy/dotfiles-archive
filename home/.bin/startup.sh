24f0ede3b0fedbed4740ba22831ece364f94a04a
Woomy <woomy@woomy.be>
2022-12-24 13:33:40 +0100

    bin/startup.sh: Drop wayland init
    I don't use sway anymore
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- home/.bin/startup.sh
+++ home/.bin/startup.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-x11_start() {
-    # Setup dualscreen
-    bash "${HOME}/.screenlayout/dualscreen.sh"
-}
-common_start() {
+# This script is responsible of starting all user services
+# Exporting environnement to systemd user services
+# Setting up wallpapers
+
+session_init() {
     # Initialise systemd services
     ## Import envirronement variables
     systemctl --user import-environment PATH DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
     ## Start base "XSession" target to start user services with correct env
     systemctl --user --no-block start XSession.target
 
-    # Common start (valid for wayland and X)
     ## Setup wallpapers
     python "${HOME}/.bin/theming/wallpapers.py"
 
-    ~/.bin/desktop-apps-fix.sh || notify-send -u critical "Desktop-apps-fix" "Failed to apply/remove wayland-specific CLI to electron apps!" # Fix .desktop files to support Wayland/X.Org
+    ## Setup dualscreen
+    sh "${HOME}/.screenlayout/dualscreen.sh"
 }
 
-if [[ "${XDG_SESSION_TYPE}" != "wayland" ]]; then
-    x11_start # X-Specific start
-fi
-
-common_start # Common start (start REQUIRED_PROCS, execute python scripts, ...)
+session_init

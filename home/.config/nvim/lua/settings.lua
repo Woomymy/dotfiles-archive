70be2043b05e09108ec6829201e0acaf3fcf3ac4
Woomymy <woomy@woomy.be>
2023-01-24 08:43:02 +0100

    config/nvim: Use spaces for makefiles
    Signed-off-by: Woomymy <woomy@woomy.be>

--- home/.config/nvim/lua/settings.lua
+++ home/.config/nvim/lua/settings.lua
@@ -13,6 +13,11 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4 -- 1 tab == 4 spaces
 vim.o.smartindent = true -- autoindent new lines
 
+-- Enforce spaces in makefiles
+vim.cmd [[
+    autocmd FileType make setlocal expandtab
+]]
+
 -- Use 2 spaces for YAML,CSS,JSON
 vim.cmd [[
     autocmd FileType javascript,typescript,yaml,css,json setlocal shiftwidth=2 tabstop=2
